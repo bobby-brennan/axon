3a31a7eef1ebbaeaef5afeaf9130fb34b0a27cf8
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-10-26 17:00:42 +0000

    logging, reset timer AFTER fire

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-global.verbose = true;
-
 var Async = require('async');
 
 var Manager = require('./lib/manager.js');
 
 var manager = new Manager();
 
+var VERBOSE = false;
 
 var nextRound = function() {
-  manager.train(10000, function() {
+  global.verbose = VERBOSE;
+  manager.train(30000, function() {
+    global.verbose = false;
     Async.series(manager.samples.map(function(sample) {
       return function(acb) {
-        manager.run(2000, sample, acb);
+        manager.run(4000, sample, acb);
       }
     }), function(err) {
       console.log('ROUND DONE');
