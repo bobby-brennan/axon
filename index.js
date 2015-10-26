374ae6e52ccd8b6c59937a1e8d5a6f13db0e01c2
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-10-26 15:47:30 +0000

    successful classification with a chain

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 global.verbose = true;
 
+var Async = require('async');
+
 var Manager = require('./lib/manager.js');
 
 var manager = new Manager();
 
-manager.train(10000);
+
+var nextRound = function() {
+  manager.train(10000, function() {
+    Async.series(manager.samples.map(function(sample) {
+      return function(acb) {
+        manager.run(2000, sample, acb);
+      }
+    }), function(err) {
+      console.log('ROUND DONE');
+    })
+  });
+}
+
+nextRound();
